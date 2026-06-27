2b8021f3f8b2835e662cced54afe61b5e9cb2c13
Tanish Bansal <tanishbansal0624@gmail.com>
2026-06-27 07:46:11 +0530

    Initial commit

---
+++ src/Config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://tanishbansal:M0AmoifjIOJo7OSe@cluster0.xveitvp.mongodb.net/devTinder");
+    console.log("MongoDB connected Successfully");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = connectDB;
