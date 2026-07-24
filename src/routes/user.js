adea1bc22c61d29c76d9387150c6539af50365fa
Tanish Bansal <tanishbansal0624@gmail.com>
2026-07-24 16:44:15 +0530

    Building feed api & pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const {userauth} = require("../middlewares/auth");
 const ConnectionRequest = require("../Models/connectionRequest");
+const User = require("../Models/user");
 const userRouter = express.Router();
 
 const User_Safe_Data = "firstName lastName photoUrl gender age skills ";
@@ -55,6 +56,48 @@ userRouter.get("/user/requests/rececived", userauth , async (req,res) =>{
     }
     catch(err){
         res.status(400).send("Error in fetching connectionRequests : " + err.message);
+    }
+});
+
+userRouter.get("/feed", userauth , async (req,res)=>{
+    try{
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit>50 ? 50:limit;
+        const skip = (page-1)*limit;
+        // find all connectionrequest send or receive
+        const connectionRequests = await ConnectionRequest.find(
+            {$or : [
+            {toUserId : loggedInUser._id},
+            {fromUserId : loggedInUser._id}
+        ]  
+        }).select("fromUserId toUserId");
+        // .populate("fromUserId","firstName")
+        // .populate("toUserId","firstName")
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach(req=>{
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+        //console.log(hideUsersFromFeed);
+
+        const users = await User.find({
+            $and : [
+            {_id : { $nin : Array.from(hideUsersFromFeed)}},
+            {_id : { $ne : loggedInUser._id}},//if the user has not make any connection req then it will not present in hideUsersFromFeed
+            ],
+        }).select(User_Safe_Data).skip(skip).limit(limit);
+
+        res.json({
+            message : "Users in the feed",
+            data : users
+        })
+    }
+    catch(err){
+        res.status(400).send("User not fount : " + err.message);
     }
 });
 
