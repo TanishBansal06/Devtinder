ee0fca8141aeb25f45efa26364cf278cf521a926
Tanish Bansal <tanishbansal0624@gmail.com>
2026-07-24 23:12:47 +0530

    Created devtinver-frontend and Installed Tailwind CSS and DaisyUI

---
+++ devtinder-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10">
+      <button className="btn btn-primary">
+        DaisyUI Working 🚀
+      </button>
+    </div>
+  );
+}
+
+export default App;
